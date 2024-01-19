b7d192d8e65e32387b87ca900a700c07a159f257
zhornyk-maria <zhornyk.maria@gmail.com>
2024-01-19 15:57:24 +0200

    add form

---
+++ src/js/2-form.js
@@ -0,0 +1,62 @@
+const STORAGE_KEY = 'feedback-form-state';
+
+function loadFromLS(key = 'empty') {
+    const data = localStorage.getItem(key); 
+
+    try {
+        const result = JSON.parse(data);
+        return result;
+    } catch {
+        return data;
+    }
+}
+
+function saveToLS(key, value) {
+    const jsonData = JSON.stringify(value);
+    localStorage.setItem(key, jsonData);
+}
+
+
+const form = document.querySelector('.feedback-form');
+const textarea = form.querySelector('textarea');
+
+form.addEventListener('input', (e) => {
+    const userEmail = form.elements.email.value;
+    const userMessage = form.elements.message.value;
+
+    const data = {
+        email: userEmail,
+        message: userMessage,
+    }
+
+    saveToLS(STORAGE_KEY, data);
+});
+
+
+function restoreData() {
+    const data = loadFromLS(STORAGE_KEY) || {};
+
+    form.elements.email.value = data.email || '';
+    form.elements.message.value = data.message || '';
+}
+
+restoreData();
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const data = loadFromLS(STORAGE_KEY) || {};
+    console.log(data);
+
+    const userEmail = form.elements.email.value.trim();
+    const userMessage = form.elements.message.value.trim();;
+    if (userEmail === '' || userMessage === '') {
+        alert('Please fill in both form elements.');
+        e.preventDefault();
+    }
+
+    localStorage.removeItem(STORAGE_KEY);
+    form.reset();
+});
+
